936bc70acb02058b068a399572c4ae125654c5c8
haeuni <hyeun9313@gmail.com>
2024-04-21 20:51:19 +0900

    Section6,7 Ex1 (hyeeun)

---
+++ src/section6/ex1/hyeeun/Item.java
@@ -0,0 +1,24 @@
+package section6.ex1.hyeeun;
+
+public class Item {
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Item(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+    public int getTotalPrice() {
+        return price * quantity;
+    }
+}
---
+++ src/section7/ex1/hyeeun/Solution.java
@@ -0,0 +1,13 @@
+package section7.ex1.hyeeun;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 4, 5};
+
+        System.out.println("sum=" + MathArrayUtils.sum(values));
+        System.out.println("average=" + MathArrayUtils.average(values));
+        System.out.println("min=" + MathArrayUtils.min(values));
+        System.out.println("max=" + MathArrayUtils.max(values));
+    }
+}
